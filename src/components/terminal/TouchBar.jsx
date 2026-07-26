5585d59d58b2792021ff8f514b51dfe6c6f9dfbe
magealexstra <magealexstra@gmail.com>
2026-07-26 01:10:44 +0000

    feat(macro): add NAV navigation & directional arrow keys preset bundle and TouchBar key mappings

--- src/components/terminal/TouchBar.jsx
+++ src/components/terminal/TouchBar.jsx
@@ -334,6 +334,26 @@ export default function TouchBar({ onKeyPress, onVoiceInput }) {
               'ArrowDown': '\x1b[B',
               'ArrowLeft': '\x1b[D',
               'ArrowRight': '\x1b[C',
+              '▲ Up': '\x1b[A',
+              '▼ Down': '\x1b[B',
+              '◀ Left': '\x1b[D',
+              '► Right': '\x1b[C',
+              '▲': '\x1b[A',
+              '▼': '\x1b[B',
+              '◀': '\x1b[D',
+              '►': '\x1b[C',
+              'PgUp': '\x1b[5~',
+              'PgDn': '\x1b[6~',
+              'Home': '\x1b[H',
+              'End': '\x1b[F',
+              'Ctrl+Left': '\x1b[1;5D',
+              'Ctrl+Right': '\x1b[1;5C',
+              'Shift+Tab': '\x1b[Z',
+              'Backspace': '\x7f',
+              'Enter': '\r',
+              'F1': '\x1bOP', 'F2': '\x1bOQ', 'F3': '\x1bOR', 'F4': '\x1bOS',
+              'F5': '\x1b[15~', 'F6': '\x1b[17~', 'F7': '\x1b[18~', 'F8': '\x1b[19~',
+              'F9': '\x1b[20~', 'F10': '\x1b[21~', 'F11': '\x1b[23~', 'F12': '\x1b[24~'
             };
             if (keyMappings[itemKey]) return keyMappings[itemKey];
 
